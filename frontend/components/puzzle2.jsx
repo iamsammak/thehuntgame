cfe9f22101e9962c9cc241d9025466e36b66823f
sheldon chang <boredsheldonxp@gmail.com>
2018-12-18 16:37:57 -0800

    submit answer functionality with correct email address
    puzzle 2 functionality

--- frontend/components/puzzle2.jsx
+++ frontend/components/puzzle2.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
 
 // Answer: See o double you
 
 class Puzzle2 extends React.Component {
+  constructor() {
+    super();
+    
+   this.state = {
+      puzzlesolved: 'no',
+    };
+     
+    }
+
+  submitanswer() {
+    var input = document.getElementById('puzzleAnswer').value
+    console.log(input)
+
+    serverresponse = socket.send('submit', {puzzle2:input})
+    
+
+    if (serverresponse === 'correct') {
+      this.setState({
+      puzzlesolved : this.state.puzzlesolved = 'yes'
+       });
+      console.log('answer is correct')
+
+      this.props.history.push('/main')
+      
+  } else {
+    var input = document.getElementById('puzzleAnswer')
+    console.log('answer is wrong')
+  }
+
   render() {
 
     return (
@@ -11,10 +41,10 @@ class Puzzle2 extends React.Component {
         <form id="puzzle2-form">
           <p>Can you write cow in 13 letters?</p>
           <p>
-            <input type="text" name="puzzleAnswer" autocomplete="off" placeholder="Type here" />
+            <input type="text" id="puzzleAnswer" autocomplete="off" placeholder="Type here" />
           </p>
           <div>
-            <button type="submit">submit</button>
+            <button type="submit" onClick={() =>this.submitanswer()}>submit</button>
           </div>
         </form>
 
