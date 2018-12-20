d2d5e48a5e90de9c13c1a25da54da5f80846a445
Andy Lau <andy@thinkingalaud.com>
2018-12-19 20:52:46 -0800

    add forgotten constants file

---
+++ frontend/constants.js
@@ -0,0 +1,9 @@
+export const green = '#009933';
+export const red = '#ff4d4d';
+
+export const xxsSpacing = 2;
+export const xsSpacing = 4;
+export const smSpacing = 8;
+export const mdSpacing = 16;
+export const lgSpacing = 32;
+export const xlSpacing = 48;
