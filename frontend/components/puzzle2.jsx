1af4299a2b4a7b89c94f29defe09b57148da6ec0
sheldon chang <boredsheldonxp@gmail.com>
2018-12-31 15:36:14 -0800

    moved listener to constructor

--- frontend/components/puzzle2.jsx
+++ frontend/components/puzzle2.jsx
@@ -4,11 +4,26 @@ import { Route, Redirect } from 'react-router-dom';
 // Answer: See o double you
 
 class Puzzle2 extends React.Component {
-  constructor() {
-    super();
-    
+  constructor(props) {
+    super(props);
+ // socketIO listen for the response and run a function to log response
+    const { socket } = this.props;
+    socket.on('submit_response', function(response) {
+      console.log(response.answer) 
+      if (response.answer == 'correct') {
+	console.log('response is correct')
+	this.setState({puzzlesolved : 'yes'})
+      } else if (response.answer === 'incorrect') {
+	console.log('answer is wrong')
+	this.setState({wronganswer: 'yes'})
+     };
+    });
+     
+console.log(socket.io.engine.id)
    this.state = {
       puzzlesolved: 'no',
+      blankanswer: 'no',
+      wronganswer: 'no'
     };
      
     }
@@ -17,21 +32,11 @@ class Puzzle2 extends React.Component {
     var input = document.getElementById('puzzleAnswer').value
     console.log(input)
 
-    sendresponse = this.props.send('submit', {puzzle2:input})
- // socketIO listen for the response and run a function to log response
-    var serverresponse = socket.on('submit_response', function(response) {
-      console.log(response.answer) 
-      if (response.answer == 'correct') {
-	console.log('response is correct')
-	this.props.historty.push('/main')
-      } else {
-	var input = document.getElementbyID('puzzleAnswer').value
-	console.log('answer is wrong')
-     };
-    });
+    this.props.send('submit', {puzzle2:input})
+ };
 
   render() {
-
+    console.log(this.state)
     return (
       <div>
         <h1>Puzzle Two</h1>
