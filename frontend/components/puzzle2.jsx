17cea737f8c7ee8d42064dcef77ac1fe5d1acb64
sheldon chang <boredsheldonxp@gmail.com>
2019-01-05 14:48:00 -0800

    implemented andy's comments

--- frontend/components/puzzle2.jsx
+++ frontend/components/puzzle2.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import styled from 'styled-components'
 
+ 
+   //if statement for incorrect answers
+   const AnswerAwareInput = styled.input`
+    border-color: ${props => props.incorrectanswer ? 'red' : '#f8f8f8t'};
+    text-color: ${props => props.incorrectanswer ? 'red' : 'black'};
+  `;
+ 
 // Answer: See o double you
 class Puzzle2 extends React.Component {
-
- constructor(props) {
+constructor(props) {
     super(props);
     
     this.state = {
@@ -21,23 +28,27 @@ class Puzzle2 extends React.Component {
   } else {
     this.setState({blankanswer : 'yes'})
     console.log('there was a blank answer')
+    this.props.send('submit', {puzzle: '1', answer: userinput})
   }
 }
   
-   
 render() {
-   //if statement for incorrect answers
-   if (this.props.incorrectanswer === true) {
-     document.getElementById("puzzleAnswer").style.borderColor = "red";
-    };
-  return ( 
+  console.log(this.props)
+  
+return ( 
 
      <div>
         <h1>Puzzle Two</h1>
         <form id="puzzle2-form">
           <p>Can you write cow in 13 letters?</p>
           <p>
-            <input type="text" id="puzzleAnswer" autocomplete="off" placeholder="Type here" />
+            <AnswerAwareInput 
+               type="text"
+               id="puzzleAnswer"
+               autocomplete="off"
+               placeholder="Type here"
+               incorrectanswer={this.props.incorrectanswer}
+            />
           </p>
           <div>
             <button type="submit" onClick={() =>this.submitanswer()}>submit</button>
