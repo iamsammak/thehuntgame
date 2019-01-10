54960ab3d38fcbd28eb4e6a0469f1bab32283dd0
Andy Lau <andy@thinkingalaud.com>
2019-01-09 23:26:52 -0800

    implement improvements to puzzle 5

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import { HashRouter } from 'react-router-dom';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faLock, faUnlock } from '@fortawesome/free-solid-svg-icons';
+import { faLock, faUnlock, faArrowLeft, faPoo, faGlassCheers, faAngry, faHorse, faAnchor, faCarrot, faCloudRain, faGlasses, faSeedling } from '@fortawesome/free-solid-svg-icons';
 
 import App from './app';
 
-library.add(faLock);
-library.add(faUnlock);
+library.add(faLock, faUnlock, faArrowLeft, faPoo, faGlassCheers, faAngry, faHorse, faAnchor, faCarrot, faCloudRain, faGlasses, faSeedling);
 
 const Root = () => {
   return (
---
+++ frontend/wrappers.jsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import { xxsSpacing, xsSpacing, smSpacing, mdSpacing, lgSpacing, xlSpacing } from './constants';
+
+export const XxsSpacing = styled.div`
+  width: ${xxsSpacing};
+  height: ${xxsSpacing};
+`;
+export const XsSpacing = styled.div`
+  width: ${xsSpacing};
+  height: ${xsSpacing};
+`;
+export const SmSpacing = styled.div`
+  width: ${smSpacing};
+  height: ${smSpacing};
+`;
+export const MdSpacing = styled.div`
+  width: ${mdSpacing};
+  height: ${mdSpacing};
+`;
+export const LgSpacing = styled.div`
+  width: ${lgSpacing};
+  height: ${lgSpacing};
+`;
+export const XlSpacing = styled.div`
+  width: ${xlSpacing};
+  height: ${xlSpacing};
+`;
