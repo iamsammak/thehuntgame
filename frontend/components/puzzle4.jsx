0d00a93dccc617387b876337591b4d4e190b9d3c
Samuel Mak <samuelswmak@gmail.com>
2019-01-12 17:31:07 -0800

    refactor handleClick per Andy's comment

--- frontend/components/puzzle4.jsx
+++ frontend/components/puzzle4.jsx
@@ -16,13 +16,16 @@ class Puzzle4 extends React.Component {
     this.clearValue = this.clearValue.bind(this);
   }
 
-  handleClick(event) {
-    let buttonNum = parseInt(event.target.value) - 1;
-    let value = this.state.value;
-    value[buttonNum] = !value[buttonNum];
-
-    this.props.send('submit', { puzzle: '4', answer: value });
-    this.setState({ value: value });
+  handleClick(idx) {
+    return () => {
+      this.setState(state => {
+        let value = state.value;
+        value[idx] = !value[idx];
+        return ({ value : value });
+      }, () => {
+        this.props.send('submit', { puzzle: '4', answer: this.state.value });
+      });
+    };
   }
 
   clearValue() {
@@ -45,15 +48,15 @@ class Puzzle4 extends React.Component {
         <br/>
         <KeypadContainer>
 
-          <Button click={this.state.value[0]} onClick={this.handleClick} value="1">1</Button>
-          <Button click={this.state.value[1]} onClick={this.handleClick} value="2">2</Button>
-          <Button click={this.state.value[2]} onClick={this.handleClick} value="3">3</Button>
-          <Button click={this.state.value[3]} onClick={this.handleClick} value="4">4</Button>
-          <Button click={this.state.value[4]} onClick={this.handleClick} value="5">5</Button>
-          <Button click={this.state.value[5]} onClick={this.handleClick} value="6">6</Button>
-          <Button click={this.state.value[6]} onClick={this.handleClick} value="7">7</Button>
-          <Button click={this.state.value[7]} onClick={this.handleClick} value="8">8</Button>
-          <Button click={this.state.value[8]} onClick={this.handleClick} value="9">9</Button>
+          <Button click={this.state.value[0]} onClick={this.handleClick(0)} >1</Button>
+          <Button click={this.state.value[1]} onClick={this.handleClick(1)} >2</Button>
+          <Button click={this.state.value[2]} onClick={this.handleClick(2)} >3</Button>
+          <Button click={this.state.value[3]} onClick={this.handleClick(3)} >4</Button>
+          <Button click={this.state.value[4]} onClick={this.handleClick(4)} >5</Button>
+          <Button click={this.state.value[5]} onClick={this.handleClick(5)} >6</Button>
+          <Button click={this.state.value[6]} onClick={this.handleClick(6)} >7</Button>
+          <Button click={this.state.value[7]} onClick={this.handleClick(7)} >8</Button>
+          <Button click={this.state.value[8]} onClick={this.handleClick(8)} >9</Button>
 
           <ButtonContainer value={name} onClick={this.handleClick}></ButtonContainer>
           <Button onClick={this.clearValue} >Clear</Button>
