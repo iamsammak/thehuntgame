fe4f4e41d12a99d1e99ab006f48e479f42891fe7
Andy Lau <614205+ihurrahi@users.noreply.github.com>
2019-01-21 22:24:08 -0800

    correct order of stages

--- frontend/helpers.js
+++ frontend/helpers.js
@@ -1,20 +1,19 @@
 export function getCurrentStage(gameState) {
-  if (gameState[1] && gameState[1].solved) {
-    return 2;
-  } else if (gameState[2] && gameState[2].solved) {
-    return 3;
-  } else if (gameState[3] && gameState[3].solved) {
-    return 4;
-  } else if (gameState[4] && gameState[4].solved) {
-    return 5;
-  } else if (gameState[5] && gameState[5].solved) {
-    return 6;
-  } else if (gameState[6] && gameState[6].solved) {
-    return 7;
+  if (gameState[8] && gameState[8].solved) {
+    return 9;
   } else if (gameState[7] && gameState[7].solved) {
     return 8;
-  } else if (gameState[8] && gameState[8].solved) {
-    return 9;
-  }
+  } else if (gameState[6] && gameState[6].solved) {
+    return 7;
+  } else if (gameState[5] && gameState[5].solved) {
+    return 6;
+  } else if (gameState[4] && gameState[4].solved) {
+    return 5;
+  } else if (gameState[3] && gameState[3].solved) {
+    return 4;
+  } else if (gameState[2] && gameState[2].solved) {
+    return 3;
+  } else if (gameState[1] && gameState[1].solved) {
+    return 2;
   return 1;
 }
