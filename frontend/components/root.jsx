5eff148415c53981a504cbf3858e5304a9f7c19b
Andy Lau <614205+ihurrahi@users.noreply.github.com>
2019-01-23 22:31:23 -0800

    make font awesome easier to work with

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -3,11 +3,11 @@ import styled from 'styled-components';
 import { HashRouter } from 'react-router-dom';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faLock, faUnlock, faArrowLeft, faArrowRight, faArrowUp, faArrowDown, faPoo, faGlassCheers, faAngry, faHorse, faAnchor, faCarrot, faCloudRain, faGlasses, faSeedling, faChessPawn, faCookieBite, faGasPump, faInfinity, faGhost, faDiceFive, faGem, faPuzzlePiece, faToilet, faPencilAlt, faMoon, faStar } from '@fortawesome/free-solid-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 import App from './app';
 
-library.add(faLock, faUnlock, faArrowLeft, faArrowRight, faArrowUp, faArrowDown, faPoo, faGlassCheers, faAngry, faHorse, faAnchor, faCarrot, faCloudRain, faGlasses, faSeedling, faChessPawn, faCookieBite, faGasPump, faInfinity, faGhost, faDiceFive, faGem, faPuzzlePiece, faToilet, faPencilAlt, faMoon, faStar);
+library.add(fas);
 
 const AppContainer = styled.div`
   display: flex;
