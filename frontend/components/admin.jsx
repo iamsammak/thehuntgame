2d602df3fd9c4a38f5a4072b50ef8adc55809aa9
sheldon chang <boredsheldonxp@gmail.com>
2019-01-26 16:22:16 -0800

    WIP

---
+++ frontend/components/admin.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+
+class Admin extends React.Component {
+  render() {  
+    return(
+      <div>
+        <h1>hello</h1>
+      </div>
+    
+    );
+  }
+}
+
+export default Admin;
