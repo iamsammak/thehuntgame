e2beab8743046e177b7598b8a0bd2109fb27ea55
Andy Lau <614205+ihurrahi@users.noreply.github.com>
2019-02-16 18:46:05 -0800

    fix icons

--- frontend/components/admin.jsx
+++ frontend/components/admin.jsx
@@ -3,8 +3,8 @@ import io from 'socket.io-client';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import faCheck from '@fortawesome/free-solid-svg-icons/faCheck';
-import faTimes from '@fortawesome/free-solid-svg-icons/faTimes';
+import { faCheck } from '@fortawesome/free-solid-svg-icons/faCheck';
+import { faTimes } from '@fortawesome/free-solid-svg-icons/faTimes';
 
 library.add(
   faCheck,
