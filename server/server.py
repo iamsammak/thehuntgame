23d000d6b39c48fe9d7e130c4d03a1e7de384142
sheldon chang <boredsheldonxp@gmail.com>
2019-02-25 20:46:32 -0800

    forgot server changes

--- server/server.py
+++ server/server.py
@@ -180,7 +180,7 @@ def submit(sid, data):
   print("answer", sid, data)
   puzzle, answer = [data.get(key) for key in ['puzzle', 'answer']]
   # TODO: error gracefully if no puzzle or answer
-  if type(answer) is list: 
+  if type(answer) is string:
     correct = answer == ANSWERS.get(str(puzzle))
   else:
     correct = answer.lower() == ANSWERS.get(str(puzzle))
