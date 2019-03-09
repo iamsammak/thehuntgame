b57813938e4c311a03a95de48b158d43867bad59
Sam <samuelswmak@gmail.com>
2019-03-09 10:56:52 -0800

    set devotion as puzzle5 answer

--- server/server.py
+++ server/server.py
@@ -39,7 +39,7 @@ ANSWERS = {
   '2': 'secret',
   '3': [5,4,3,2,1],
   '4': [False, False, True, True, False, False, False, False, True, False],
-  '5': '',
+  '5': 'devotion',
   '6': '',
   '7': [False, True, True, False, False, True, True, False, False, True],
   '8': '',
