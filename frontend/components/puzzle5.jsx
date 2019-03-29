7565d301f1894abe8a6a2a553b110d39d045edfd
Andy Lau <614205+ihurrahi@users.noreply.github.com>
2019-03-29 07:50:37 -0700

    involve christine in some prompts

--- frontend/components/puzzle5.jsx
+++ frontend/components/puzzle5.jsx
@@ -14,7 +14,7 @@ class Puzzle5 extends React.Component {
     return (
       <div>
         <SpeechBubble personId={personId}>
-          Earlier this morning, Chris gave us a piece of paper and said to hold onto it. We had the smart idea of tearing it apart and splitting up the pieces among us...maybe that piece of paper has something to do with the key.
+          Earlier this morning, Christine gave us a piece of paper and said to hold onto it. We had the smart idea of tearing it apart and splitting up the pieces among us...maybe that piece of paper has something to do with the key.
         </SpeechBubble>
         <Puzzle5Clue clue="clue7" personId={personId} />
         <p>
